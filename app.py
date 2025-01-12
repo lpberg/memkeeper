9835bbc2693796d616bea80b8f7891bc8001c1f5
Leif Berg <leif.berg@gmail.com>
2025-01-11 20:28:55 -0600

    Refactored api and upload structure to live in Memory class instead of app

--- app.py
+++ app.py
@@ -12,15 +12,12 @@ def flaskprint(str):
 	print(str, file=sys.stderr)
 
 # Create MemoryCollection Object (helps read/write/manage Memory objects)
-# "memories" is the name of a directory where files will be read/written
 mc = MemoryCollection("memories")
 
 # Create Flask Application
 app = Flask(__name__)
 
-# Set upload directory
-# TODO: enforce image file extensions
-ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+# Set upload directory - flask requires use of static dir
 app.config['UPLOAD_FOLDER'] = './static/uploads/images'
 
 # Redirect root to view
@@ -33,48 +30,45 @@ def render_index_page():
 def render_view_page():
 	return render_template("view.html",memories = mc.get_data())
 
-# Helper function that creates directories and saves images from add memory function
-def process_images(request_files,id):
-	img_paths = []
-	if 'file1' in request_files:
-		if not os.path.exists(os.path.join(app.config['UPLOAD_FOLDER'],str(id))):
-			os.makedirs(os.path.join(app.config['UPLOAD_FOLDER'],str(id)))
-		file = request_files['file1']
-		if file:
-			path = os.path.join(app.config['UPLOAD_FOLDER'],str(id),file.filename)
-			file.save(path)
-			img_paths.append(path)
-	return img_paths
-
 # Create Route to Add a Memory
-@app.route('/add_memory', methods=['POST'])
+@app.route('/memory/add', methods=['POST'])
 def add_memory():
 	if request.method == "POST":
-		request_data = {}
-		request_data["id"] = str(uuid.uuid1())
-		request_data["title"] = request.form["title"]
-		request_data["desc"] = request.form["desc"]
-		request_data["img_paths"] = process_images(request.files,request_data["id"])
-		memory = Memory(request_data)
+		memory = Memory(request.form,request.files,app.config['UPLOAD_FOLDER'])
 		mc.add(memory)
 		return jsonify({'Flask Server':"Memory added"})
 
 # Create Route to get Data for a Memory (via a Memory ID)
-@app.route('/get_memory', methods=['POST'])
+@app.route('/memory/get', methods=['POST'])
 def get_memory():
 	request_data = request.get_json()
 	data = mc.get(request_data["id"]).get_data()
 	return jsonify(data)
 
 # Create Route to Update a Memory
-# TODO - redo update call using new data structure
-@app.route('/update_memory', methods=['POST'])
-def update_memeory():
+@app.route('/memory/update', methods=['POST'])
+def update_memory():
 	request_data = request.get_json()
 	memory = mc.get(request_data["id"])
 	memory.update(request_data)
 	mc.writeFile(memory)
 	return jsonify({'Flask Server':request_data["id"]+" updated"})
+
+# Create Route to Add Images to Existing Memory
+@app.route('/image/add', methods = ['POST'])
+def add_images_to_memory():
+	if request.method == 'POST':
+		memory = mc.get(request.form["id"])
+		added_images = memory.add_images(request.files)
+	return jsonify({"img_paths":added_images})
+
+# Create Route to Remove Image from Existing Memory
+@app.route("/image/remove", methods = ['POST'])
+def remove_image_from_memory():
+	if request.method == 'POST':
+		memory = mc.get(request.form["id"])
+		removal_successful = memory.remove_image(request.form["img_path"])
+	return jsonify({request.form["img_path"]:removal_successful})
 
 # Run Flask Application on Port 5005
 if __name__ == "__main__":
